045df0eacc57d1c22a85f4e253aa449e4119e295
Tao Xiang <46348901+yunshu67@users.noreply.github.com>
2021-04-15 20:30:56 +0200

    updates

--- Classify 4/test.py
+++
@@ -1,6 +0,0 @@
-from skimage.io import ImageCollection
-from skimage import io
-blank_arr = ImageCollection('Blank/Blank*.png')
-img = io.imread('Blank_aug/Blank_aug _0_15.png')
-print(blank_arr[5].shape)
-print(img.shape)
--- notebooks-master/README.md
+++
@@ -1,2 +0,0 @@
-# Python notebooks
-Code material for the youtube channel: https://www.youtube.com/channel/UC8fwFSM3zKpa-huYkubwEpA
